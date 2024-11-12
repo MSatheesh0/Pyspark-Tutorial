c4dedc7e122d51714a4f837928a580c8b19ed13c
MSatheesh0 <satheesh2005satheesh@gmail.com>
2024-11-12 21:12:13 +0530

    Add files via upload

---
+++ creatiing_a_spark_session.py
@@ -0,0 +1,18 @@
+import os
+
+os.environ['SPARK_HOME'] = r"C:\Spark"
+os.environ['PYSPARK_DRIVER_PYTHON'] = 'jupyter'
+os.environ['PYSPARK_DRIVER_PYTHON_OPTS'] = 'lab'
+os.environ['PYSPARK_PYTHON'] = 'python'
+
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+    .appName("MyApp") \
+    .config("spark.executor.memory", "2g") \
+    .config("spark.sql.shuffle.partitions", "4") \
+    .getOrCreate()
+
+spark
+
+spark.stop()
